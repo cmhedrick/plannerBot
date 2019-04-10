5f4da8b009d06add24c6434095de24a369b2e866
cmhedrick <christopher.m.hedrick@gmail.com>
2019-04-10 06:58:40 -0400

    fix weird import issue with models?

--- sqlhelper/sqlhelper.py
+++ sqlhelper/sqlhelper.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-from models import Chat
+from .models import Chat
 
 
 class SQLHelper:
